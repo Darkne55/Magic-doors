c852f54607432afc1aed627786fb75d1f30eac7e
notebook\Darkness <d4rkn355d14n4@gmail.com>
2017-03-24 14:32:57 +0200

    All keys in inventory

--- Test/Assets/Scripts/orangeKeyChest.cs
+++ Test/Assets/Scripts/orangeKeyChest.cs
@@ -21,18 +21,15 @@ public class orangeKeyChest : MonoBehaviour {
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == player.tag)
-        {
-
-            
+        {           
             int count = inventary.list.Count;
             for (int i = 0; i < count; i++)
             {
                 if (inventary.list[i].sprite == titleKey)
-                {
-                    Debug.Log("Hello");
-
+                {                 
                     GetComponent<chestFirstControl>().enabled = true;
                     GetComponent<chestFirstControl>().OpenChestWithKey();
+                   // inventary.list.Remove(inventary.list[i]);
                 }
             }
         }
